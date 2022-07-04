3fd332c36f39fee5ed27ad2ffef608f134028b45
optikrus <optikrus61@gmail.com>
2022-07-04 20:39:07 +0300

    add requirements.txt

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.core.handlers.wsgi import WSGIRequest
+from django.db.models import QuerySet
 
-# Register your models here.
+from api.models import Data
+
+from dadata import Dadata
+
+
+@admin.register(Data)
+class AddressAdmin(admin.ModelAdmin):
+    result = ['result__icontains']
+    search_fields = ['result']
+
+    # убираем кнопку "Добавить" в админке
+    def has_add_permission(self, request):
+        return False
+
+    def get_search_results(self, request: WSGIRequest, queryset: QuerySet, search_term):
+        if request.get_full_path().startswith('/admin/api/data/?q='):
+            token = "ab17900dad15e70b5340449bb528835b05b011da"
+            secret = "3df71589945f847ca7de68c087b81391bd4d4663"
+            dadata = Dadata(token, secret)
+            response = dadata.clean("address", search_term)
+            Data.objects.get_or_create(result=response)
+        return queryset, True
--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Data(models.Model):
+    result = models.JSONField('Город', null=True)
+
+    def __str__(self):
+        return f'{self.result["result"]}'
+
+    class Meta:
+        verbose_name_plural = 'Поиск'
